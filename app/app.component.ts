58ea64796925bb165b18683ccd2f0fcbf3c2759d
Jonathan Cousins <evolutionxbox@gmail.com>
2016-12-23 13:45:50 +0000

    Added ng switch example

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: ``
+  template: `
+    <ul class="nav nav-pills">
+      <li [class.active]="viewMode == 'map'"><a (click)="viewMode = 'map'">Map View</a></li>
+      <li [class.active]="viewMode == 'list'"><a (click)="viewMode = 'list'">List View</a></li>
+    </ul>
+
+    <div [ngSwitch]="viewMode">
+      <template [ngSwitchCase]="'map'">Map View Content</template>
+      <template [ngSwitchCase]="'list'">List View Content</template>
+    </div>
+  `
 })
 
 export class AppComponent {
+  viewMode = 'map';
 }
