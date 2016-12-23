690b9400d24754c88aa4e356925cb1afd35b2ce2
Jonathan Cousins <evolutionxbox@gmail.com>
2016-12-23 13:57:31 +0000

    Added ng for example

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: ``
+  template: `
+    <ul>
+     <li *ngFor="let course of courses; let i = index">
+      <span>{{ i + 1 }} {{ course }}</span>
+     </li>
+    </ul>
+  `
 })
 
 export class AppComponent {
+  courses = ['Course1', 'Course2', 'Course3'];
 }
