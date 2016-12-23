8af3e32f43b1afd2469f9d3cc067f4301fd7bff3
Jonathan Cousins <evolutionxbox@gmail.com>
2016-12-23 14:10:14 +0000

    Added interpolated string pipe examples

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: ``
+  template: `
+    {{ course.title | uppercase | lowercase }}
+    <br>
+    {{ course.students | number }}
+    <br>
+    {{ course.rating | number:'2.2-2' }}
+    <br>
+    {{ course.price | currency:'GBP':true:'2.2-2' }}
+    <br>
+    {{ course.releaseDate | date:'dd MM yyyy' }}
+    <br>
+    {{ course | json }}
+  `
 })
 
 export class AppComponent {
+  course = {
+    title: "Angular 2 for Beginners",
+    rating: 4.9745,
+    students: 5981,
+    price: 10.00,
+    releaseDate: new Date(2016, 3, 1)
+  }
 }
