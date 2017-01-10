3cf69a7a5de038d204b2728a96e9adbbea3af1d8
Jonathan Cousins <evolutionxbox@gmail.com>
2017-01-10 11:53:36 +0000

    Added loop to show off how the 'elvis operator' works

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,18 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <ul>
+    <ul *ngFor="let task of tasks">
       <li>Title: {{ task.title }}</li>
-      <li>Assigned to: {{ task.assignee.name }}</li>
+      <li>Assigned to: {{ task.assignee?.name }}</li>
     </ul>
   `
 })
 
 export class AppComponent {
-  task = {
+  tasks = [{
     title: "Review applications",
+    assignee: null
+  },
+  {
+    title: "Learn Angular",
     assignee: {
       name: "Jon"
     }
-  };
+  }];
 }
