58d5e6ab8588a5670ec62f9b99b1695a48d9940d
Jonathan Cousins <evolutionxbox@gmail.com>
2017-01-10 11:33:47 +0000

    Added button to ngClass example

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: ``
+  template: `
+    <button
+      [style.backgroundColor]="canSave ? 'blue' : 'gray'"
+      [style.color]="canSave ? 'white' : 'black"
+      [style.fontWeight]="canSave ? 'bold' : 'normal'">
+      Submit
+    </button>
+  `
 })
 
 export class AppComponent {
+  canSave = true;
 }
