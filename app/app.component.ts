96c7774daa0f16985c0f35a618b3091f94f2f078
Jonathan Cousins <evolutionxbox@gmail.com>
2017-01-10 14:36:56 +0000

    Added zippy component example

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: ``
+  template: `
+    <zippy title="This is the title">
+      Here is the content
+    </zippy>
+    <zippy title="Who can see my stuff?">
+      I can
+    </zippy>
+  `
 })
 
 export class AppComponent {
