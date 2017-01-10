df8d63f6b1baf34258168881170ea5eb96b7f2d0
Jonathan Cousins <evolutionxbox@gmail.com>
2017-01-10 12:19:32 +0000

    Added example showing how to use ng-content element

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<bs-panel></bs-panel>`
+  template: `
+    <bs-panel>
+      <div class="heading">The Heading</div>
+      <div class="body">The Body</div>
+      <div class="body">The is another body</div>
+    </bs-panel>
+  `
 })
 
 export class AppComponent {
