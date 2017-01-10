247a3eb98a257b8f3bc1fd832fd3e63676dcaba8
Jonathan Cousins <evolutionxbox@gmail.com>
2017-01-10 11:38:02 +0000

    Added start example code for the 'elvis operator'

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: ``
+  template: `
+    <ul>
+      <li>Title: {{ task.title }}</li>
+      <li>Assigned to: {{ task.assignee.name }}</li>
+    </ul>
+  `
 })
 
 export class AppComponent {
+  task = {
+    title: "Review applications",
+    assignee: {
+      name: "Jon"
+    }
+  };
 }
